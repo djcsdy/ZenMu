37375324702cff15723c725abe2c76cd7e6f01df
Anathema <professionalnihilist@gmail.com>
2012-05-13 09:17:42 +0100

    Add email field to user

--- ZenMu/Models/ZenMuUser.cs
+++ ZenMu/Models/ZenMuUser.cs
@@ -9,6 +9,7 @@ namespace ZenMu.Models
     {
         public string Username { get; set; }
         public string Password { get; set; }
+        public string Email { get; set; }
         public string[] Roles { get; set; }
         public bool IsDeleted { get; set; }
     }
